19605b522d30e9e7cf85b0f81298351f1729aefb
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2025-05-15 23:14:25 +0300

    Avoid repeating FST compilation

--- FomaInterface.php
+++ FomaInterface.php
@@ -2,13 +2,13 @@
 class FomaTransducer
 {
 	private string $binaryPath;
-	function __construct($transducerPath, $compile=true, $verbose=false)
+	function __construct($transducerPath, $compile=false, $verbose=false)
 	{
 		$directory = dirname($transducerPath);
 		$name = basename($transducerPath, '.foma');
 		$this->binaryPath = realpath($directory).DIRECTORY_SEPARATOR.$name.'.bin';
 		$oldDir = getcwd();
-		if ($compile) {
+		if (!file_exists($this->binaryPath) || $compile) {
 			chdir($directory);
 			$result = `foma -e "source $name.foma" -e "push $name" -e "save stack $name.bin" -e "exit"`;
 			if ($verbose) {
