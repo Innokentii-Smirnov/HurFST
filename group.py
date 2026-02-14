cb9b46607b86a07e9b71bc6d186050879ec2f81c
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-02-15 00:42:01 +0100

    Add a script for selecting columns and grouping the data by the input column

---
+++ group.py
@@ -0,0 +1,19 @@
+import sys
+from os import path
+import pandas as pd
+COLUMNS = ['transcription', 'segmentation', 'translation', 'morph_tag', 'pos']
+directory = sys.argv[1]
+assert path.exists(directory), 'The output directory does not exist'
+selected_columns = sys.argv[2:4]
+words = pd.read_csv('data/words.csv', names=COLUMNS)
+df = words.loc[:, selected_columns]
+print(len(df))
+df.drop_duplicates(selected_columns, inplace=True)
+print(len(df))
+print(df.head())
+input_column, output_column = selected_columns
+grouped = df.groupby(input_column).agg({output_column: lambda values: ', '.join(sorted(values))})
+print(len(grouped))
+print(grouped.head())
+grouped.to_csv(path.join(directory, 'input.txt'), sep='\t', columns=[], header=False)
+grouped.to_csv(path.join(directory, 'corr.txt'), sep='\t', header=False, index=False)
